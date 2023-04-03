22016e50cfbe7463c7d142bd1271838836559062
yeah1tsmark <marcwafula635@gmail.com>
2023-04-03 10:34:12 +0300

    authentication android

---
+++ app/src/main/java/com/example/authentication_android_mark/Register_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.authentication_android_mark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Register_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
